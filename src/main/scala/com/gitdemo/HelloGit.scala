e90397c82400b82692b84af3fe3424f1b67fba56
Rajesh Santha <rajesh.santha29@gmail.com>
2023-04-18 23:31:50 +0530

    added some deleted code

--- src/main/scala/com/gitdemo/HelloGit.scala
+++ src/main/scala/com/gitdemo/HelloGit.scala
@@ -7,5 +7,15 @@ println(convertDecimalToBinaryString(8))
 
   }
 
-}
+  def convertDecimalToBinaryString (n: Int): String = {
+    var x = n
+    var binaryString = ""
+    while (x > 0) {
+      binaryString += (x % 2)
+      x = x / 2
+    }
+    binaryString.reverse
+
+  }
+//test
 }
