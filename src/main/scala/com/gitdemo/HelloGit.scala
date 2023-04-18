106b13ce968499dec7ec67d2f839ff4624a89e22
Rajesh Santha <rajesh.santha29@gmail.com>
2023-04-18 23:18:38 +0530

    deleted the code

--- src/main/scala/com/gitdemo/HelloGit.scala
+++ src/main/scala/com/gitdemo/HelloGit.scala
@@ -7,23 +7,5 @@ println(convertDecimalToBinaryString(8))
 
   }
 
-  def convertDecimalToBinaryString (n: Int): String = {
-    var x = n
-    var binaryString = ""
-    while (x > 0) {
-      binaryString += (x % 2)
-      x = x / 2
-    }
-    binaryString.reverse
-
-  }
-//test
-def convertDecimalToBinaryFunctional (n: Int): String = {
-  Iterator.iterate(n)(_ / 2)
-    .takeWhile(_ > 0)
-    .map(_ % 2)
-    .mkString
-    .reverse
 }
-
 }
